27d021cb6f2b7e4dbe41113156b6f340a6f308f8
AaronKollasch <awkollasch@gmail.com>
2016-11-19 23:03:20 -0600

    made server directory and updated server file

--- Server/auth_srv.py
+++ Server/auth_srv.py
@@ -1,10 +1,10 @@
 import flask
+import flask_login
 from flask import Flask, jsonify
 from flask import json
 from flask import render_template
 from flask import request
 import multiprocessing
-
 
 app = Flask(__name__)
 users = {'Joe': {'pw': 'Kirkland'}}
@@ -25,16 +25,19 @@ def hello():
             users[usrname] = {'pw': pw}
             return render_template('index.html', registered=1)
     return "You should not be here. Sorry 'bout that"
+    
+@app.route('/auth' , methods=['POST'])
+def auth():
+    auth_data = request.data
+    print auth_data
+    #render_template('index.html', registered=1)
+    return "Thanks Comrade!"
+    #received_json_data = json.loads(flask.request.)
+    #pw = flask.request.form['pw']
 
 @app.route('/')
 def index():
     return render_template('index.html')
 
 if __name__ == '__main__':
-    app.run()
-
-@app.route('/auth' , methods=['POST'])
-def auth():
-    return "Thanks Comrade!"
-    #received_json_data = json.loads(flask.request.)
-    #pw = flask.request.form['pw']
+    app.run(debug=True)
